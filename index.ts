e6e7f5c67bd117996ca153c4db270f6f4ee9c3c1
Alimkhan <magomadovv@mail.ru>
2024-07-26 20:47:29 +0300

    End

--- index.ts
+++ index.ts
@@ -54,3 +54,52 @@ const me = {
 const createdNoTry = (o: object | null ): void => {}
 
 createdNoTry({object: "Ничего не возвращает!"})
+
+// Создайте переменную типа any и присвойте ей значение вашего любимого цвета.
+
+let o: any = "Зеленый"
+
+// Создайте список, содержащий элементы разных типов данных (строки, числа, объекты и т.д.), и присвойте его переменной типа any.
+
+const listAny: any = ["Hello", 123, { name: "Yohan", age: 24 }, true];
+
+// Напишите функцию, которая принимает переменную типа any и ничего не возвращает.
+
+const funAny = (vle: any): void => {
+  console.log(vle);
+}
+
+// Создайте тип с именем Person, который содержит свойства name типа string и age типа number.
+
+type Person = {
+  name: string,
+  age: number
+};
+
+const personss: Person = {
+  name: "alex",
+  age: 20
+};/*Это написал для себя, (знаю, что не надо)*/
+
+// Создайте тип с именем Product, который содержит свойства name типа string, price типа number и inStock типа boolean
+
+type Product = {
+  name: string,
+  price: number,
+  inStock: boolean
+};
+
+// Создайте тип с именем User, который содержит свойства id типа number, username типа string и email типа string.
+
+type User = {
+  id: number,
+  username: string,
+  email: string
+};
+
+// Создайте тип Account, который содержит свойства id типа number и balance типа number.
+
+type Account = {
+  id: number,
+  balance: number
+}
